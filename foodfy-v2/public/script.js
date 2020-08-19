543aca9cf4d3ab9179fe5365c1653baae9094697
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-19 17:53:51 +0100

    up

--- foodfy-v2/public/script.js
+++ foodfy-v2/public/script.js
@@ -1,27 +1,9 @@
-const cards = document.querySelectorAll(".card");
-const hide = document.querySelector(".hide");
-const content = document.querySelector(".content");
-const close = document.querySelector(".close");
+const cards = document.querySelectorAll(".card")
 
 for(let card of cards){
     card.addEventListener("click", function(){
-        const img = card.querySelector("img");
-        const h1 = card.querySelector("h3");
-        const p = card.querySelector("span");
-
-        hide.classList.add("active");
-
-        content.querySelector("img").src = img.src;
-        content.querySelector("h1").innerHTML = h1.textContent;
-        content.querySelector("p").innerHTML = p.textContent;
-
+        const id = card.getAttribute("id")
+        window.location.href = "/receitas/" + id
     })
 };
 
-document.querySelector(".close").addEventListener("click", function(){
-    hide.classList.remove('active')
-
-    content.querySelector("img").src = "";
-    content.querySelector("h1").innerHTML = "";
-    content.querySelector("p").innerHTML = "";
-})
